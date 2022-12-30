9595018f5a5153ab2f1f211e93ebe619aed9052f
rizqiindah03 <rizqiindah32@gmail.com>
2022-12-30 17:52:51 +0700

    membuat method static void

--- BinarySearchTree_067/Program.cs
+++ BinarySearchTree_067/Program.cs
@@ -114,5 +114,48 @@ namespace BinarySearchTree_067
                 Console.Write(ptr.info + " ");
             }
         }
+        static void Main(string[] args)
+        {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\n Menu");
+                Console.WriteLine("1. Implement insert operations");
+                Console.WriteLine("2. Perform inorder tranversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine(" 5. Exit");
+                Console.Write("\n Enter ypur choice  (1-5) :");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch(ch)
+                {
+                    case '1':
+                        {
+                            Console.Write("Enter a word : ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break ;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid option");
+                            break;
+                        }
+                }
+            }
+        }
     }
 }
